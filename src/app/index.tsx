31836678494ad5d2e656d0f525bcbe30d74e97ba
FelixYSW <therealfelixyap@gmail.com>
2026-07-21 12:44:57 +0800

    minimum height changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -69,10 +69,10 @@ export default function App() {
 
   const snapPoints = useMemo(() => {
     // Keep array lengths identical to prevent BottomSheet index bugs on Web
-    if (appState === 'default') return ['15%', '63%'];
-    if (appState === 'preview') return ['31%', '31%'];
-    if (appState === 'navigating') return ['20%', '20%'];
-    return ['15%', '15%'];
+    if (appState === 'default') return ['13%', '50%'];
+    if (appState === 'preview') return ['21%', '21%'];
+    if (appState === 'navigating') return ['22%', '20%'];
+    return ['20%', '20%'];
   }, [appState]);
 
   const handlePlaceSelected = async (placeName: string, lat: number, lng: number, address: string = '') => {
